14418b7c9ca205fe227d1a969a87d826e79907d9
lihaibo <lihaibo@okay.cn>
2022-06-15 18:44:42 +0800

    fix:初始化

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import {createPinia} from 'pinia'
+import {createPinia} from './pinia'
 
 const pinia = createPinia()
 const app = createApp(App);
